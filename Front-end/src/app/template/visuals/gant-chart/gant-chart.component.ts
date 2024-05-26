14c09e5339d20c39e6cdaae48cee1141505d4d04
Ilias Zaazaa <77403753+ilias-stack@users.noreply.github.com>
2024-05-26 23:29:59 +0100

    fkit hmari

--- Front-end/src/app/template/visuals/gant-chart/gant-chart.component.ts
+++ Front-end/src/app/template/visuals/gant-chart/gant-chart.component.ts
@@ -1,22 +1,31 @@
-import { Component, Input } from '@angular/core';
-import { ChartData, ChartOptions, ChartType } from 'chart.js';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import {
+  Chart,
+  ChartData,
+  ChartOptions,
+  ChartType,
+  registerables,
+} from 'chart.js';
+import 'chartjs-adapter-moment';
+import { sample } from 'rxjs';
+import { GantTaskModel } from './task.model';
 
 @Component({
   selector: 'app-gant-chart',
   templateUrl: './gant-chart.component.html',
   styleUrl: './gant-chart.component.css',
 })
-export class GantChartComponent {
+export class GantChartComponent implements OnInit {
   @Input() barChartLabels!: string[];
   @Input() barChartData!: number[];
   @Input() barChartTitle!: string;
 
   public barChartAll: ChartData<'line'> = {
-    labels: ['', '', '', ''],
+    labels: ['a', 's', 'f', '2', 'a', 's', 'f', '2'],
     datasets: [
       {
         label: '',
-        data: [10, 20, 30, 10],
+        data: [10, 20, 30, 10, 10, 20, 30, 10],
         backgroundColor: [
           '#EF4444',
           '#4F46E5',
@@ -33,19 +42,48 @@ export class GantChartComponent {
     ],
   };
 
+  sampleData: GantTaskModel[] = [
+    { label: 'Task 1', start: '2022-02-01', end: '2022-02-03' },
+    { label: 'Task 2', start: '2022-02-04', end: '2022-02-06' },
+    { label: 'Task 3', start: '2022-02-07', end: '2022-02-10' },
+  ];
+
   public barChartOptions: ChartOptions = {
     indexAxis: 'y',
     responsive: true,
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
   };
+
+  constructor() {
+    Chart.register(...registerables);
+  }
+
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
 
-  constructor() {}
-
   ngOnInit() {
-    // this.barChartAll.labels = this.barChartLabels;
-    // this.barChartAll.datasets[0].data = this.barChartData;
-    // this.barChartAll.datasets[0].label = this.barChartTitle;
+    const datasetData = this.sampleData.map((task) => [
+      { x: new Date(task.start).getTime(), y: task.label },
+      { x: new Date(task.end).getTime(), y: task.label },
+    ]);
+    this.barChartAll.datasets[0].data = datasetData.flat().map((itemi) => {
+      return itemi.x;
+    });
+    this.barChartAll.labels = datasetData.flat().map((itemi) => {
+      return itemi.y;
+    });
   }
 }
---
+++ Front-end/src/app/template/visuals/gant-chart/task.model.ts
@@ -0,0 +1,5 @@
+export interface GantTaskModel {
+  label: string;
+  start: string;
+  end: string;
+}
