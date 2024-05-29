5b3f2dcf2f2493ee87f540414517f4dd944af917
Ilias Zaazaa <77403753+ilias-stack@users.noreply.github.com>
2024-05-29 11:11:02 +0100

    gantt added

---
+++ Front-end/src/app/models/task.model.ts
@@ -0,0 +1,11 @@
+export interface Task {
+  id: number;
+  taskType: string;
+  startDate: string;
+  estimatedEndDate: string;
+  taskProgress: string;
+  employeeName: string;
+  projectName: string;
+  description: string;
+  endDate: string;
+}
